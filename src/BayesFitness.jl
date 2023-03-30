16cc9bad55efea70c522c04480c955bf2aa286d8
mrazomej <manuel.razo.m@gmail.com>
2023-03-29 17:47:17 -0700

    export function

--- src/BayesFitness.jl
+++ src/BayesFitness.jl
@@ -1,7 +1,11 @@
 module BayesFitness
 
 module model
+# Export mean fitness model
+export mean_fitness_neutrals_lognormal
+
 include("model.jl")
+
 end # model submodule
 
 end # BayesFitness module
