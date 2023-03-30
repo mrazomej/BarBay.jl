c2cba4463a5680add6913c7453bc211aa9bbe85c
mrazomej <manuel.razo.m@gmail.com>
2023-03-30 08:37:51 -0700

    add first version of mutant fitness turing model

--- src/model.jl
+++ src/model.jl
@@ -17,10 +17,10 @@ export mean_fitness_neutrals_lognormal
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 
 @doc raw"""
-    mean_fitness_neutrals_lognormal(r̲ₜ, r̲ₜ₊₁, α̲, sₜ_prior, σₜ_prior)
+    mean_fitness_neutrals_lognormal(r̲ₜ, r̲ₜ₊₁, α̲, s_prior, σ_prior)
 
 `Turing.jl` model to sample the posterior for a single population mean fitness
-value sₜ, given the raw barcode counts. 
+value `sₜ`, given the raw barcode counts. 
 
 # Model 
 For this inference, we can write Bayes theorem as
@@ -78,8 +78,8 @@ with zero reads) to compute ``\gamma_t^{(n)}``.
 
 The user defines the distribution parameters as:
 - ``\underline{\alpha}_t``: `α̲`.
-- ``[\mu_{\bar{s}_t}, \sigma_{\bar{s}_t}]``: `sₜ_prior`.
-- ``[\mu_{\sigma_t}, \sigma_{\sigma_t}]``: `σₜ_prior`.
+- ``[\mu_{\bar{s}_t}, \sigma_{\bar{s}_t}]``: `s_prior`.
+- ``[\mu_{\sigma_t}, \sigma_{\sigma_t}]``: `σ_prior`.
 
 # Arguments
 - `r̲ₜ::Vector{Int64}`: Raw counts for **neutral** lineages and the cumulative
@@ -91,25 +91,25 @@ The user defines the distribution parameters as:
 - `α̲::Vector{Float64}`: Parameters for Dirichlet prior distribution.
 
 ## Optional arguments
-- `sₜ_prior::Vector{Real}=[0.0, 2.0]`: Parameters for the mean fitness prior
+- `s_prior::Vector{Real}=[0.0, 2.0]`: Parameters for the mean fitness prior
   distribution π(sₜ).
-- `σₜ_prior::Vector{Real}=[0.0, 1.0]`: Parameters for the nuisance standard
+- `σ_prior::Vector{Real}=[0.0, 1.0]`: Parameters for the nuisance standard
   deviation parameter prior distribution π(σₜ).
-- `σₜ_trunc::Real=0.0`: Value at which truncate the normal distribution to
+- `σ_trunc::Real=0.0`: Value at which truncate the normal distribution to
   define it as a half-normal.
 """
 Turing.@model function mean_fitness_neutrals_lognormal(
     r̲ₜ::Vector{Int64},
     r̲ₜ₊₁::Vector{Int64},
     α̲::Vector{Float64};
-    sₜ_prior::Vector{Real}=[0.0, 2.0],
-    σₜ_prior::Vector{Real}=[0.0, 1.0],
-    σₜ_trunc::Real=0.0
+    s_prior::Vector{Real}=[0.0, 2.0],
+    σ_prior::Vector{Real}=[0.0, 1.0],
+    σ_trunc::Real=0.0
 )
     # Prior on mean fitness sₜ
-    sₜ ~ Turing.Normal(sₜ_prior...)
+    sₜ ~ Turing.Normal(s_prior...)
     # Prior on LogNormal error σₜ
-    σₜ ~ Turing.truncated(Turing.Normal(σₜ_prior...); lower=σₜ_trunc)
+    σₜ ~ Turing.truncated(Turing.Normal(σ_prior...); lower=σ_trunc)
 
     # Frequency distribution from Multinomial-Dirichlet model
     f̲ₜ ~ Turing.Dirichlet(α̲ .+ r̲ₜ)
@@ -141,10 +141,10 @@ Turing.@model function mean_fitness_neutrals_lognormal(
 end # @model function
 
 @doc raw"""
-    mean_fitness_neutrals_lognormal(r̲ₜ, r̲ₜ₊₁, α̲, sₜ_prior, σₜ_prior)
+    mean_fitness_neutrals_lognormal(r̲ₜ, r̲ₜ₊₁, α̲, s_prior, σ_prior)
 
 `Turing.jl` model to sample out of the posterior for a single population mean
-fitness value sₜ, given the raw barcode counts. Note: this `method` allows the
+fitness value `sₜ`, given the raw barcode counts. Note: this `method` allows the
 use of any prior distribution, different from the Normal and Half-Normal priors.
 
 # Model 
@@ -204,16 +204,16 @@ The user defines the distribution parameters as:
 - ``\underline{\alpha}_t``: `α̲`.
 
 # Arguments
-- `r̲ₜ::Array{Int64}`: Raw counts for **neutral** lineages and the cumulative
+- `r̲ₜ::Vector{Int64}`: Raw counts for **neutral** lineages and the cumulative
   counts for mutant lineages at time `t`. NOTE: The last entry of the array must
   be the sum of all of the counts from mutant lineages.
-- `r̲ₜ₊₁::Array{Int64}`: Raw counts for **neutral** lineages and the cumulative
+- `r̲ₜ₊₁::Vector{Int64}`: Raw counts for **neutral** lineages and the cumulative
   counts for mutant lineages at time `t + 1`. NOTE: The last entry of the array
   must be the sum of all of the counts from mutant lineages.
-- `α̲::Array{Float64}`: Parameters for Dirichlet prior distribution.
-- `sₜ_prior::Distributions.ContinuousUnivariateDistribution`: Parametrized
+- `α̲::Vector{Float64}`: Parameters for Dirichlet prior distribution.
+- `s_prior::Distributions.ContinuousUnivariateDistribution`: Parametrized
   univariate continuous distribution for the prior on the mean fitness π(sₜ).
-- `σₜ_prior:::Distributions.ContinuousUnivariateDistribution`: Parametrized
+- `σ_prior:::Distributions.ContinuousUnivariateDistribution`: Parametrized
   univariate continuous distribution for the prior on the nuisance standard
   deviation of the log-normal likelihood π(σₜ).
 """
@@ -221,13 +221,13 @@ Turing.@model function mean_fitness_neutrals_lognormal(
     r̲ₜ::Vector{Int64},
     r̲ₜ₊₁::Vector{Int64},
     α̲::Vector{Float64},
-    sₜ_prior::Distributions.ContinuousUnivariateDistribution,
-    σₜ_prior::Distributions.ContinuousUnivariateDistribution;
+    s_prior::Distributions.ContinuousUnivariateDistribution,
+    σ_prior::Distributions.ContinuousUnivariateDistribution;
 )
     # Prior on mean fitness sₜ
-    sₜ ~ sₜ_prior
+    sₜ ~ s_prior
     # Prior on LogNormal error σₜ
-    σₜ ~ σₜ_prior
+    σₜ ~ σ_prior
 
     # Frequency distribution from Multinomial-Dirichlet model
     f̲ₜ ~ Turing.Dirichlet(α̲ .+ r̲ₜ)
@@ -261,3 +261,73 @@ end # @model function
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 # Mutant fitness π(s⁽ᵐ⁾ | data)
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+@doc raw"""
+
+`Turing.jl` model to sample out of the posterior distribution for a single
+mutant fitness value `s⁽ᵐ⁾`, given the raw barcode counts and the
+parametrization of the population mean fitness distribution.
+
+# Arguments
+- `r̲⁽ᵐ⁾::Vector{Int64}`: Mutant `m` raw barcode counts time-series. Note: this
+  vector must be the same length as `r̲⁽ᶜ⁾`. This means that each entry
+  `r̲⁽ᵐ⁾[i]` contains the number of reads from barcode `m` at time `i`.
+- `R̲::Vector{Int64}`: time-series of Raw **total** reads. This means that entry
+  `R̲[i]` contains the total number of reads obtained at time `i`.
+- `α̲::Vector{Float64}`: Parameters for Beta prior distribution.
+- `μ_sₜ::Vector{Float64}`: Array with the time-series mean values of the
+  population mean fitness. This means entry `μ_sₜ[i]` contains the inferred mean
+  value of the population mean fitness for time `i`, assuming sₜ[i] ~
+  Normal(μ_sₜ[i], σ_sₜ[i]).
+- `σ_sₜ::Vector{Float64}`: Array with the time-series values of the population
+  mean fitness standard deviation. This means entry `σ_sₜ[i]` contains the
+  inferred value of the standard deviation of the population mean fitness at
+  time `i`, assuming sₜ[i] ~ Normal(μ_sₜ[i], σ_sₜ[i]).
+
+## Optional arguments
+- `s_prior::Vector{Real}=[0.0, 2.0]`: Parameters for the mutant fitness prior
+  distribution π(s⁽ᵐ⁾).
+- `σ_prior::Vector{Real}=[0.0, 1.0]`: Parameters for the nuisance standard
+  deviation parameter prior distribution π(σ⁽ᵐ⁾).
+- `σ_trunc::Real=0.0`: Value at which truncate the normal distribution to define
+  it as a half-normal.
+"""
+Turing.@model function mutant_fitness_lognormal(
+    r̲⁽ᵐ⁾::Vector{Int64},
+    R̲::Vector{Int64},
+    α̲::Vector{Float64},
+    μ_sₜ::Vector{Float64},
+    σ_sₜ::Vector{Float64};
+    s_prior::Vector{Real}=[0.0, 2.0],
+    σ_prior::Vector{Real}=[0.0, 1.0],
+    σ_trunc::Real=0.0
+)
+    # Prior on mutant fitness s⁽ᵐ⁾
+    s⁽ᵐ⁾ ~ Turing.Normal(s_prior...)
+    # Prior on LogNormal error σ⁽ᵐ⁾ 
+    σ⁽ᵐ⁾ ~ Turing.truncated(Turing.Normal(σ_prior...); lower=σ_trunc)
+
+    # Population mean fitness values
+    s̲ₜ ~ Turing.MvNormal(μ_sₜ, LinearAlgebra.Diagonal(σ_sₜ .^ 2))
+
+    # Initialize array to store frequencies
+    f̲⁽ᵐ⁾ = Vector{Float64}(undef, length(r̲⁽ᵐ⁾))
+
+    # Frequency distribution for each time point
+    for i in eachindex(r̲⁽ᵐ⁾)
+        f̲⁽ᵐ⁾[i] ~ Turing.Beta(α̲[1] + r̲⁽ᵐ⁾[i], α̲[2] + (R̲[i] - r̲⁽ᵐ⁾[i]))
+    end # for
+
+    # Compute frequency ratios
+    γ̲⁽ᵐ⁾ = f̲⁽ᵐ⁾[2:end] ./ f̲⁽ᵐ⁾[1:end-1]
+
+    # Sample posterior for frequency ratio. Since it is a sample over a
+    # generated quantity, we must use the @addlogprob! macro
+    Turing.@addlogprob! Turing.logpdf(
+        Turing.MvLogNormal(
+            s⁽ᵐ⁾ .- s̲ₜ,
+            LinearAlgebra.I(length(s̲ₜ)) .* σ⁽ᵐ⁾^2
+        ),
+        γ̲⁽ᵐ⁾
+    )
+end # @model function
