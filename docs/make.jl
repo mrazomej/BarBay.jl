a88a2e8c53872d4697b0f13aa6b8e9d3610c7d60
mrazomej <manuel.razo.m@gmail.com>
2023-04-11 10:32:49 -0700

    setting GitHub Actions

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@ using Documenter
 using BayesFitness
 
 makedocs(
-    sitename = "BayesFitness",
-    format = Documenter.HTML(),
-    modules = [BayesFitness]
+    sitename="BayesFitness",
+    format=Documenter.HTML(),
+    modules=[BayesFitness]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo="github.com/mrazomej/BayesFitness.jl.git"
+)
