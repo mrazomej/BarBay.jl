50f6b3c1f27f0a454018282a7783697edccefabd
mrazomej <manuel.razo.m@gmail.com>
2023-04-11 12:15:10 -0700

    update test run to infer full dataset

--- test/mcmc_mutant_fitness_multithread_test.jl
+++ test/mcmc_mutant_fitness_multithread_test.jl
@@ -22,15 +22,18 @@ data = CSV.read("$(git_root())/test/data/data_example_01.csv", DF.DataFrame)
 
 # Infer mean fitness distributions
 mean_fitness_dist = BayesFitness.stats.gaussian_prior_mean_fitness(
-    BayesFitness.utils.var_jld2_to_df("./output/", "test", :sₜ)
+    BayesFitness.utils.var_jld2_to_df("./output/", "data_01_meanfitness", :sₜ)
 )
+
+##
 
 # Define function parameters
 param = Dict(
     :data => data,
-    :n_steps => 10,
+    :n_walkers => 3,
+    :n_steps => 1_000,
     :outputdir => "./output/",
-    :outputname => "test_mutant_mcmc",
+    :outputname => "data_01_mutantfitness",
     :model => BayesFitness.model.mutant_fitness_lognormal,
     :model_kwargs => Dict(
         :α => BayesFitness.stats.beta_prior_mutant(
@@ -40,19 +43,10 @@ param = Dict(
         :σ_s̄ => mean_fitness_dist[2],
     )
 )
+
 ##
 
 # Run inference
 BayesFitness.mcmc.mcmc_mutant_fitness_multithread(; param...)
 
 ##
-
-mutant_fitness_lognormal(
-    r̲⁽ᵐ⁾::Vector{Int64},
-    R̲::Vector{Int64};
-    α::Vector{Float64},
-    μ_sₜ::Vector{Float64},
-    σ_sₜ::Vector{Float64},
-    s_prior::Vector{Real}=[0.0, 2.0],
-    σ_prior::Vector{Real}=[0.0, 1.0],
-    σ_trunc::Real=0.0
