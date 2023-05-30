a811d3662be23350383e07bbd53797e924f686d6
mrazomej <manuel.razo.m@gmail.com>
2023-05-30 14:21:29 -0700

    big changes to the functions in utils now that I use native MCMCChains routines to convert to DF

--- src/utils.jl
+++ src/utils.jl
@@ -15,255 +15,129 @@ import JLD2
 ##
 
 @doc raw"""
-    var_jld2_to_df(dir, pattern, varname, chainname)
+    `concat_chains(chains, var_pattern, id_str)`
 
-Function that takes `.jld2` files in `dir` with `pattern` and extracts a single
-variable into a dataframe. This function is useful to extract, for example, the
-chains for each inference of the population mean fitness over multiple time
-points.
+Function that concatenates multiple `MCMCChains.Chains` objects into a single
+one. This function takes a vector of `MCMCChains.Chains` as inputs, extracts the
+variables that match the patterns in the array `var_pattern`, and appends all
+extracted variables into a single chain adding a pattern of the form
+`[$(id_str)i]`, where `i` is the file number. For example, if two chains contain
+a variable named `var`, the new chain returned by this function names them as
+`var[f1]` and `var[f2]` if `id_str=f`.
 
-NOTE: All chains from which samples will be extracted must have the same number
-of samples.
+NOTE: All chains must have the same number of samples to be concatenated.
 
 # Arguments
-- `dir::String`: Directory where file(s) with MCMC chains are stored.
-- `pattern::String`: Pattern common among all files to process. NOTE: This is
-  use in the `Glob.glob` command to locate all `jld2` files from which to
-  extract the chains.
-- `varname::Symbol`: Name of variable in chain object to extract.
+- `chains::Vector{<:MCMCChains.Chains}`: Vector with the chains to be
+  concatenated into a single chain.
+- `var_pattern::Vector{Symbol}`: Patterns that variables must follow to be
+  extracted from the chain. For example, if several variables are named
+  `var[1]`, `var[2]`, etc, providing a pattern [`var`] extracts all of them,
+  while providing `var[1]` extracts only the one that perfectly matches this
+  pattern.
 
-## Optional Arguments
-- `chainname::String="chain"`: String defining the dictionary key on the `.jld2`
-  file to extract the MCMC chain.
+## Optional arguments
+- `id_str::String=f`: String to be attached to the variable names that
+  identifies the different chains being concatenated. For example, if 4 chains
+  are being concatenated, each repeated variable will be named `var[$(id_str)i]`
+  to distinguish each of them.
 
 # Returns
-- `DataFrames.DataFrame`: DataFrame containing all variable samples⸺multiple
-  chains are collapsed into a single column⸺one variable per column.
+- `MCMCChains.Chains`: Chain with the requested variables from multiple files
+  concatenated into a single object.
 """
-function var_jld2_to_df(
-    dir::String, pattern::String, varname::Symbol, chainname::String="chain",
+function concat_chains(
+    chains::Vector{<:MCMCChains.Chains},
+    var_pattern::Vector{Symbol};
+    id_str::String="f"
 )
-    # List files
-    files = sort(Glob.glob("$(dir)/*$(pattern)*.jld2"))
-
-    # Extract variable chains
-    chains = [vec(Matrix(JLD2.load(f)[chainname][varname])) for f in files]
-
     # Check that all chains have the same number of samples
-    if length(unique(length.(chains))) > 1
+    if length(unique([length(range(chn)) for chn in chains])) > 1
         error("All chains must have the same number of samples")
     end # if
 
-    # concatenate chains into DataFrame
-    return DF.DataFrame(
-        hcat(chains...),
-        ["$(varname)_$(i)" for i in eachindex(files)]
-    )
-end # function
+    # Initialize array to save names
+    varnames = []
 
-@doc raw"""
-    var_jld2_to_df(dir, pattern, varnames, chainname)
+    # Initialize array to save chains
+    chain_samples = []
 
-Function that takes `.jld2` files in `dir` with `pattern` and extracts a single
-variable into a dataframe. This function is useful to extract, for example, the
-chains for each inference of the population mean fitness over multiple time
-points.
+    # Loop through files
+    for (i, chn) in enumerate(chains)
+        # Extract names into single vector
+        names_ = reduce(
+            vcat, [MCMCChains.namesingroup(chn, x) for x in var_pattern]
+        )
+        # Convert names to string and append pattern
+        push!(
+            varnames, String.(names_) .* "[$(id_str)$(i)]"
+        )
 
-NOTE: All chains from which samples will be extracted must have the same number
-of samples.
-
-# Arguments
-- `dir::String`: Directory where file(s) with MCMC chains are stored.
-- `pattern::String`: Pattern common among all files to process. NOTE: This is
-  use in the `Glob.glob` command to locate all `jld2` files from which to
-  extract the chains.
-- `varnames::Vectlr{Symbol}`: Names of variable in chain object to extract.
-
-## Optional Arguments
-- `chainname::String="chain"`: String defining the dictionary key on the `.jld2`
-  file to extract the MCMC chain.
-
-# Returns
-- `DataFrames.DataFrame`: DataFrame containing all variable samples⸺multiple
-  chains are collapsed into a single column⸺one variable per column.
-"""
-function var_jld2_to_df(
-    dir::String,
-    pattern::String,
-    varnames::Vector{Symbol};
-    chainname::String="chain"
-)
-    # List files
-    files = sort(Glob.glob("$(dir)/*$(pattern)*.jld2"))
-
-    # Initialize dataframe to save output
-    df = DF.DataFrame()
-
-    # Loop through variables
-    for (i, varname) in enumerate(varnames)
-
-        # Extract variable chains
-        chains = [vec(Matrix(JLD2.load(f)[chainname][varname])) for f in files]
-
-        # Check that all chains have the same number of samples
-        if length(unique(length.(chains))) > 1
-            error("All chains must have the same number of samples")
-        end # if
-
-        # Append dataframe with variables
-        df = hcat(df,
-            DF.DataFrame(
-                hcat(chains...),
-                ["$(varname)_$(i)" for i in eachindex(files)]
-            )
+        # Extract variable chains into an array with the right format used to
+        # build an MCMCChains.Chains object
+        push!(
+            chain_samples,
+            cat(Array(chn[names_], append_chains=false)..., dims=3)
         )
     end # for
 
-    return df
-
-end # function
-
-@doc raw"""
-    var_jld2_to_df(files, varname, chainname)
-
-Function that takes `.jld2` files in `dir` with `pattern` and extracts a single
-variable into a dataframe. This function is useful to extract, for example, the
-chains for each inference of the population mean fitness over multiple time
-points.
-
-NOTE: All chains from which samples will be extracted must have the same number
-of samples.
-
-# Arguments
-- `files::Vectlr{String}`: List of `jld2` files form which to extract the MCMC
-  chain.
-- `varname::Symbol`: Name of variable in chain object to extract.
-
-## Optional Arguments
-- `chainname::String="chain"`: String defining the dictionary key on the `.jld2`
-  file to extract the MCMC chain.
-
-# Returns
-- `DataFrames.DataFrame`: DataFrame containing all variable samples⸺multiple
-  chains are collapsed into a single column⸺one variable per column.
-"""
-function var_jld2_to_df(
-    files::Vector{String}, varname::Symbol, chainname::String="chain",
-)
-    # Extract variable chains
-    chains = [vec(Matrix(JLD2.load(f)[chainname][varname])) for f in files]
-
-    # Check that all chains have the same number of samples
-    if length(unique(length.(chains))) > 1
-        error("All chains must have the same number of samples")
-    end # if
-
-    # concatenate chains into DataFrame
-    return DF.DataFrame(
-        hcat(chains...),
-        ["$(varname)_$(i)" for i in eachindex(files)]
+    # Return MCMCChains.Chains object with all files appended
+    return MCMCChains.Chains(
+        cat(chain_samples..., dims=2),
+        reduce(vcat, varnames)
     )
 end # function
 
 @doc raw"""
-    var_jld2_concat(dir, pattern, varname)
+    `jld2_concat_chains(dir, file_patern, chains, var_pattern, id_str)`
 
-Function that takes `.jld2` files in `dir` with `pattern` and extracts a single
-variable to concatentate into a single chain object. This function is useful to
-extract variables with the same name on multiple `.jld2` files and compile them
-into a single `MCMCChains.Chains` object.
+Convenient function that peforms the same concatenation as
+`BayesFitness.utils.concat_chains` but giving a directory and a file pattern for
+`jld2` files storing the chains. This function reads all files in `dir` that
+have the pattern `file pattern`, obtaining a list of `MCMCChains.Chains` as
+inputs. It then extracts the variables that match the patterns in the array
+`var_pattern`, and appends all extracted variables into a single chain adding a
+pattern of the form `[$(id_str)i]`, where `i` is the file number. For example,
+if two chains contain a variable named `var`, the new chain returned by this
+function names them as `var[f1]` and `var[f2]` if `id_str=f`.
 
-NOTE: All chains from which samples will be extracted must have the same number
-of samples.
+NOTE: All chains must have the same number of samples to be concatenated.
 
 # Arguments
 - `dir::String`: Directory where file(s) with MCMC chains are stored.
-- `pattern::String`: Pattern common among all files to process. NOTE: This is
+- `file_pattern::String`: Pattern common among all files to process. NOTE: This is
   use in the `Glob.glob` command to locate all `jld2` files from which to
   extract the chains.
-- `varname::Symbol`: Name of variable in chain object to extract.
+- `var_pattern::Vector{Symbol}`: Patterns that variables must follow to be
+  extracted from the chain. For example, if several variables are named
+  `var[1]`, `var[2]`, etc, providing a pattern [`var`] extracts all of them,
+  while providing `var[1]` extracts only the one that perfectly matches this
+  pattern.
 
-## Optional Arguments
-- `chainname::String="chain"`: String defining the dictionary key on the `.jld2`
-  file to extract the MCMC chain.
+## Optional arguments
+- `id_str::String=f`: String to be attached to the variable names that
+  identifies the different chains being concatenated. For example, if 4 chains
+  are being concatenated, each repeated variable will be named `var[$(id_str)i]`
+  to distinguish each of them.
 
 # Returns
-- `MCMCChains.Chains`: Chain containing all variable samples.
+- `MCMCChains.Chains`: Chain with the requested variables from multiple files
+  concatenated into a single object.
+- `chainname::String="chain"`: String defining the dictionary key on the `.jld2`
+file to extract the MCMC chain.
 """
-function var_jld2_concat(
-    dir::String, pattern::String, varname::Symbol, chainname::String="chain",
+function jld2_concat_chains(
+    dir::String,
+    file_pattern::String,
+    var_pattern::Vector{Symbol};
+    id_str::String="f"
+    chainname::String="chain",
 )
     # List files
-    files = sort(Glob.glob("$(dir)/*$(pattern)*.jld2"))
+    files = sort(Glob.glob("$(dir)/*$(file_pattern)*.jld2"))
 
     # Extract variable chains
-    chains = [Matrix(JLD2.load(f)[chainname][varname]) for f in files]
+    chains = [JLD2.load(f)[chainname] for f in files]
 
-    # Check that all chains have the same number of samples
-    if length(unique(length.(chains))) > 1
-        error("All chains must have the same number of samples")
-    end # if
-
-    # concatenate into single data chain. NOTE: The chains axes have to be
-    # rearranged to make it match the dimensions expected by MCMCChains.Chains
-    return MCMCChains.Chains(
-        permutedims(cat(chains..., dims=3), [1, 3, 2]),
-        [Symbol(String(varname) * "[$x]") for x = 1:length(chains)]
-    )
-end # function
-
-@doc raw"""
-    chain_to_df(chain, varnames)
-
-Function that extracts the variables in `varnames` from the `chain` object and
-concatenates them into a `DataFrame`. Multiple chains are piled on top of each
-other.
-
-# Arguments
-- `chain::MCMCChains.Chains`: Chain with variables to be converted to dataframe
-
-# Optional arguments
-- `varnames::Union{Vector{Symbol}, Nothing}=nothing`: Names of the variables in
-  the chain to be extracted. These can be either the full variable name or
-  patterns to extract multi-dimensional variables. If `nothing` is given, all
-  parameters are extracted into a dataframe.
-
-# Returns
-`DataFrames.DataFrame`: Data frame where each column represents all the MCMC
-samples for a single variable (multiple chains are stack on top of each other)
-"""
-function chain_to_df(
-    chain::MCMCChains.Chains, varnames::Union{Vector{Symbol},Nothing}=nothing
-)
-    # Extract parameter names
-    chain_param = MCMCChains.names(chain, :parameters)
-
-    # Check if varnames were given
-    if typeof(varnames) == Nothing
-        # Extract and return selected variables in dataframe
-        return DF.DataFrame(
-            [vec(Matrix(chain[var])) for var in chain_param],
-            chain_param
-        )
-    else
-        # Convert chain_param into string
-        chain_str = String.(chain_param)
-
-        # Initialize list of variables to extract
-        vars = Symbol[]
-        # Loop through varnames
-        for v in varnames
-            # Search and append parameters that match pattern
-            push!(
-                vars,
-                chain_param[occursin.(String(v), chain_str)]...
-            )
-        end # for
-
-        # Extract and return selected variables in dataframe
-        return DF.DataFrame(
-            [vec(Matrix(chain[var])) for var in unique(vars)],
-            vars
-        )
-    end # if
+    return concat_chains(chains, var_pattern; id_str=id_str)
 end # function
