a4558b579eb42a7bc1ce2869b835e99738e68e3c
mrazomej <manuel.razo.m@gmail.com>
2023-06-06 17:51:46 -0700

    update local model definition to match the one from the model module

--- test/mcmc_inference_test.jl
+++ test/mcmc_inference_test.jl
@@ -17,9 +17,7 @@ import Random
 # Import MCMC-related packages
 import Turing
 using ReverseDiff
-using Zygote
 import MCMCChains
-using DynamicHMC
 
 # Import libraries to manipulate data
 import DataFrames as DF
@@ -38,10 +36,65 @@ import ColorSchemes
 Random.seed!(42)
 ##
 
+@doc raw"""
+    fitness_lognormal(R̲̲, R̲̲⁽ⁿ⁾, R̲̲⁽ᵐ⁾, n̲ₜ; s_pop_prior, σ_pop_prior, s_mut_prior, σ_mut_prior, λ_prior)
+
+`Turing.jl` model to sample the joint posterior distribution for a competitive
+fitness experiment.
+
+# Model
+`[write model here]`
+
+# Arguments
+- `R̲̲⁽ⁿ⁾::Matrix{Int64}`: `T × N` matrix where `T` is the number of time points
+  in the data set and `N` is the number of neutral lineage barcodes. Each column
+  represents the barcode count trajectory for a single neutral lineage.
+  **NOTE**: The model assumes the rows are sorted in order of increasing time.
+- `R̲̲⁽ᵐ⁾::Matrix{Int64}`: `T × M` matrix where `T` is the number of time points
+  in the data set and `M` is the number of mutant lineage barcodes. Each column
+  represents the barcode count trajectory for a single mutant lineage. **NOTE**:
+  The model assumes the rows are sorted in order of increasing time.
+- `R̲̲::Matrix{Int64}`:: `T × B` matrix, where `T` is the number of time points
+  in the data set and `B` is the number of barcodes. Each column represents the
+  barcode count trajectory for a single lineage. **NOTE**: This matrix **must**
+  be equivalent to `hcat(R̲̲⁽ⁿ⁾, R̲̲⁽ᵐ⁾)`. The reason it is an independent input
+  parameter is to avoid the `hcat` computation within the `Turing` model.
+- `n̲ₜ::Vector{Int64}`: Vector with the total number of barcode counts for each
+  time point. **NOTE**: This vector **must** be equivalent to computing
+  `vec(sum(R̲̲, dims=2))`. The reason it is an independent input parameter is to
+  avoid the `sum` computation within the `Turing` model.
+
+## Optional Keyword Arguments
+- `s_pop_prior::Vector{Float64}=[0.0, 2.0]`: Vector with the correspnding
+    parameters (`s_pop_prior[1]` = mean, `s_pop_prior[2]` = standard deviation)
+    for a Normal prior on the population mean fitness values. **NOTE**: This
+    method assigns the same prior to **all** population mean fitness to be
+    inferred.
+- `σ_pop_prior::Vector{Float64}=[0.0, 1.0]`: Vector with the correspnding
+    parameters (`σ_pop_prior[1]` = mean, `σ_pop_prior[2]` = standard deviation)
+    for a Log-Normal prior on the population mean fitness error utilized in the
+    log-likelihood function. **NOTE**: This method assigns the same prior to
+    **all** population mean fitness errors to be inferred.
+- `s_mut_prior::Vector{Float64}=[0.0, 2.0]`: Vector with the correspnding
+    parameters (`s_mut_prior[1]` = mean, `s_mut_prior[2]` = standard deviation)
+    for a Normal prior on the mutant fitness values. **NOTE**: This method
+    assigns the same prior to **all** mutant fitness values to be inferred.
+- `σ_mut_prior::Vector{Float64}=[0.0, 1.0]`: Vector with the correspnding
+    parameters (`σ_mut_prior[1]` = mean, `σ_mut_prior[2]` = standard deviation)
+    for a Log-Normal prior on the mutant fitness error utilized in the
+    log-likelihood function. **NOTE**: This method assigns the same prior to
+    **all** mutant fitness error values to be inferred.
+- `λ_prior::Vector{Float64}=[3.0, 3.0]`: Vector with the corresponding
+  parameters (`λ_prior[1]` = mean, `λ_prior[2]` = standard deviation) for a
+  Log-Normal prior on the λ parameter in the Poisson distribution. The λ
+  parameter can be interpreted as the mean number of barcode counts since we
+  assume any barcode count `n⁽ᵇ⁾ ~ Poisson(λ⁽ᵇ⁾)`. **NOTE**: This method assigns
+    the same prior to **all** mutant fitness error values to be inferred.
+"""
 Turing.@model function fitness_lognormal(
-    R̲̲::Matrix{Int64},
     R̲̲⁽ⁿ⁾::Matrix{Int64},
     R̲̲⁽ᵐ⁾::Matrix{Int64},
+    R̲̲::Matrix{Int64},
     n̲ₜ::Vector{Int64};
     s_pop_prior::Vector{Float64}=[0.0, 2.0],
     σ_pop_prior::Vector{Float64}=[0.0, 1.0],
@@ -49,67 +102,88 @@ Turing.@model function fitness_lognormal(
     σ_mut_prior::Vector{Float64}=[0.0, 1.0],
     λ_prior::Vector{Float64}=[3.0, 3.0]
 )
-    # Prior on population mean fitness
+    ## %%%%%%%%%%%%%% Population mean fitness  %%%%%%%%%%%%%% ##
+
+    # Prior on population mean fitness P(s̲ₜ)
     s̲ₜ ~ Turing.MvNormal(
         repeat([s_pop_prior[1]], size(R̲̲⁽ⁿ⁾, 1) - 1),
         LinearAlgebra.I(size(R̲̲⁽ⁿ⁾, 1) - 1) .* s_pop_prior[2] .^ 2
     )
-    # Prior on LogNormal error σ̲ₜ
+    # Prior on LogNormal error P(σ̲ₜ)
     σ̲ₜ ~ Turing.MvLogNormal(
         repeat([σ_pop_prior[1]], size(R̲̲⁽ⁿ⁾, 1) - 1),
         LinearAlgebra.I(size(R̲̲⁽ⁿ⁾, 1) - 1) .* σ_pop_prior[2] .^ 2
     )
 
-    # Prior on mutant fitness s̲⁽ᵐ⁾
+    ## %%%%%%%%%%%%%% Mutant fitness  %%%%%%%%%%%%%% ##
+
+    # Prior on mutant fitness P(s̲⁽ᵐ⁾)
     s̲⁽ᵐ⁾ ~ Turing.MvNormal(
         repeat([s_mut_prior[1]], size(R̲̲⁽ᵐ⁾, 2)),
         LinearAlgebra.I(size(R̲̲⁽ᵐ⁾, 2)) .* s_mut_prior[2] .^ 2
     )
-    # Prior on LogNormal error σ̲⁽ᵐ⁾ 
+    # Prior on LogNormal error P(σ̲⁽ᵐ⁾)
     σ̲⁽ᵐ⁾ ~ Turing.MvLogNormal(
         repeat([σ_mut_prior[1]], size(R̲̲⁽ᵐ⁾, 2)),
         LinearAlgebra.I(size(R̲̲⁽ᵐ⁾, 2)) .* σ_mut_prior[2] .^ 2
     )
 
-    # Prior on λ parameters
+
+    ## %%%%%%%%%%%%%% Barcode frequencies %%%%%%%%%%%%%% ##
+
+    # Prior on Poisson distribtion parameters P(λ)
     Λ̲̲ ~ Turing.MvLogNormal(
-        repeat([λ_prior[1]], size(R̲̲⁽ⁿ⁾, 1) * (size(R̲̲⁽ⁿ⁾, 2) + size(R̲̲⁽ᵐ⁾, 2))),
-        LinearAlgebra.I(size(R̲̲⁽ⁿ⁾, 1) * (size(R̲̲⁽ⁿ⁾, 2) + size(R̲̲⁽ᵐ⁾, 2))) .*
+        repeat([λ_prior[1]], length(R̲̲)),
+        LinearAlgebra.I(length(R̲̲)) .*
         λ_prior[2]^2
     )
 
-    # Reshape λ parameters to fit the matrix format
-    Λ̲̲ = reshape(Λ̲̲, size(R̲̲⁽ⁿ⁾, 1), (size(R̲̲⁽ⁿ⁾, 2) + size(R̲̲⁽ᵐ⁾, 2)))
+    # Reshape λ parameters to fit the matrix format. Note: The Λ̲̲ array is
+    # originally sampled as a vector for the `Turing.jl` samplers to deal with
+    # it. But reshaping it to a matrix simplifies the computation of frequencies
+    # and frequency ratios.
+    Λ̲̲ = reshape(Λ̲̲, size(R̲̲)...)
 
-    # Compute frequencies πₜ
+    # Compute barcode frequencies from Poisson parameters
     F̲̲ = Λ̲̲ ./ sum(Λ̲̲, dims=2)
 
-    # Compute frequency ratios γₜ
+    # Compute frequency ratios between consecutive time points.
     Γ̲̲ = F̲̲[2:end, :] ./ F̲̲[1:end-1, :]
-    # Split neutral and mutant frequency ratios
+
+    # Split neutral and mutant frequency ratios. Note: the @view macro means
+    # that there is not allocation to memory on this step.
     Γ̲̲⁽ⁿ⁾ = @view Γ̲̲[:, 1:size(R̲̲⁽ⁿ⁾, 2)]
     Γ̲̲⁽ᵐ⁾ = @view Γ̲̲[:, size(R̲̲⁽ⁿ⁾, 2)+1:end]
 
-    # Prob nₜ | λ̲ₜ
+    # Prob of total number of barcodes read given the Poisosn distribution
+    # parameters P(nₜ | λ̲ₜ)
     n̲ₜ ~ Turing.arraydist([Turing.Poisson(sum(Λ̲̲[t, :])) for t = 1:size(R̲̲⁽ⁿ⁾, 1)])
 
-    # Loop through time
+    # Loop through time points
     for t = 1:size(R̲̲⁽ⁿ⁾, 1)
-        # Prob of reads given parameters R̲ₜ | nₜ, f̲ₜ.
-        R̲̲[t, :] ~ Turing.Multinomial(n̲ₜ[t], F̲̲[t, :])
+        # Prob of reads given parameters P(R̲ₜ | nₜ, f̲ₜ). Note: We add the
+        # check_args=false option to avoid the recurrent problem of
+        # > Multinomial: p is not a probability vector.
+        # due to rounding errors
+        R̲̲[t, :] ~ Turing.Multinomial(n̲ₜ[t], F̲̲[t, :]; check_args=false)
     end # for
 
+    ## %%%%%%%%%%%%%% Log-Likelihood functions %%%%%%%%%%%%%% ##
 
+    # Sample posterior for neutral lineage frequency ratio. Since it is a sample
+    # over a generated quantity, we must use the @addlogprob! macro
+    # P(γₜ⁽ⁿ⁾| sₜ, σₜ)
     Turing.@addlogprob! Turing.logpdf(
         Turing.MvLogNormal(
-            -1 .* repeat(s̲ₜ, size(Γ̲̲⁽ⁿ⁾, 2)),
+            -1.0 .* repeat(s̲ₜ, size(Γ̲̲⁽ⁿ⁾, 2)),
             LinearAlgebra.Diagonal(repeat(σ̲ₜ .^ 2, size(Γ̲̲⁽ⁿ⁾, 2)))
         ),
         Γ̲̲⁽ⁿ⁾[:]
     )
 
-    # Sample posterior for frequency ratio. Since it is a sample over a
-    # generated quantity, we must use the @addlogprob! macro
+    # Sample posterior for nutant lineage frequency ratio. Since it is a sample
+    # over a generated quantity, we must use the @addlogprob! macro
+    # P(γₜ⁽ᵐ⁾ | s⁽ᵐ⁾, σ⁽ᵐ⁾, s̲ₜ)
     Turing.@addlogprob! Turing.logpdf(
         Turing.MvLogNormal(
             # Build vector for fitness differences
@@ -131,33 +205,30 @@ data = CSV.read("$(git_root())/test/data/data_example_01.csv", DF.DataFrame)
 
 ##
 
+# Define relevant column names
 time_col = :time
 id_col = :barcode
 count_col = :count
+neutral_col = :neutral
+
+# %%%%%%%%%%% Neutral barcodes data %%%%%%%%%%% ##
 
 # Re-extract unique time points
 timepoints = sort(unique(data[:, time_col]))
 
 # Group data by unique mutant barcode
-data_group = DF.groupby(data[.!data[:, :neutral], :], id_col)
+data_group = DF.groupby(data[data[:, neutral_col], :], id_col)
 
 # Check that all barcodes were measured at all points
 if any([size(d, 1) for d in data_group] .!= length(timepoints))
-    error("Not all barcodes have reported counts in all time points")
+    error("Not all neutral barcodes have reported counts in all time points")
 end # if
 
 # Extract keys
 data_keys = [k[String(id_col)] for k in keys(data_group)]
 
-# Extract total number of barcodes per timepoint
-R_tot = DF.combine(DF.groupby(data, time_col), count_col => sum)
-# Sort dataframe by time
-DF.sort!(R_tot, time_col)
-# Extract sorted counts
-R̲ = R_tot[:, Symbol(String(count_col) * "_sum")]
-
 # Initialize array to save counts for each mutant at time t
-R_mut = Matrix{Int64}(
+R̲̲⁽ⁿ⁾ = Matrix{Int64}(
     undef, length(timepoints), length(data_group)
 )
 
@@ -166,34 +237,24 @@ for (i, d) in enumerate(data_group)
     # Sort data by timepoint
     DF.sort!(d, time_col)
     # Extract data
-    R_mut[:, i] = d[:, count_col]
+    R̲̲⁽ⁿ⁾[:, i] = d[:, count_col]
 end # for
 
-# R_mut = R_mut[:, 1:10]
-
-
-##
+## %%%%%%%%%%% Mutant barcodes data %%%%%%%%%%% ##
 
 # Group data by unique mutant barcode
-data_group = DF.groupby(data[data[:, :neutral], :], id_col)
+data_group = DF.groupby(data[.!data[:, neutral_col], :], id_col)
 
 # Check that all barcodes were measured at all points
 if any([size(d, 1) for d in data_group] .!= length(timepoints))
-    error("Not all barcodes have reported counts in all time points")
+    error("Not all mutant barcodes have reported counts in all time points")
 end # if
 
 # Extract keys
 data_keys = [k[String(id_col)] for k in keys(data_group)]
 
-# Extract total number of barcodes per timepoint
-R_tot = DF.combine(DF.groupby(data, time_col), count_col => sum)
-# Sort dataframe by time
-DF.sort!(R_tot, time_col)
-# Extract sorted counts
-R̲ = R_tot[:, Symbol(String(count_col) * "_sum")]
-
 # Initialize array to save counts for each mutant at time t
-R_neut = Matrix{Int64}(
+R̲̲⁽ᵐ⁾ = Matrix{Int64}(
     undef, length(timepoints), length(data_group)
 )
 
@@ -202,16 +263,16 @@ for (i, d) in enumerate(data_group)
     # Sort data by timepoint
     DF.sort!(d, time_col)
     # Extract data
-    R_neut[:, i] = d[:, count_col]
+    R̲̲⁽ᵐ⁾[:, i] = d[:, count_col]
 end # for
 
-##
+## %%%%%%%%%%% Total barcodes data %%%%%%%%%%% ##
 
-# Concatenate data
-R_tot = hcat(R_neut, R_mut)
+# Concatenate neutral and mutant data matrices
+R̲̲ = hcat(R̲̲⁽ⁿ⁾, R̲̲⁽ᵐ⁾)
 
 # Compute total counts for each run
-nₜ = vec(sum(R_tot, dims=2))
+n̲ₜ = vec(sum(R̲̲, dims=2))
 
 ##
 
@@ -224,10 +285,10 @@ Turing.setrdcache(true)
 
 # Sample chain
 @time chain = Turing.sample(
-    fitness_lognormal(R_tot, R_neut, R_mut, nₜ),
+    fitness_lognormal(R̲̲⁽ⁿ⁾, R̲̲⁽ᵐ⁾, R̲̲, n̲ₜ),
     Turing.NUTS(0.65),
     Turing.MCMCThreads(),
-    1_000,
+    10,
     4
 )
 
