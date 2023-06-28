9ca5cfa2528864e4259b0f68034265cad1434f58
mrazomej <manuel.razo.m@gmail.com>
2023-06-28 13:22:22 -0700

    update script to perform joint inference

--- test/mcmc_joint_inference_test.jl
+++ test/mcmc_joint_inference_test.jl
@@ -35,103 +35,31 @@ Turing.setadbackend(:reversediff)
 # Allow system to generate cache to speed up computation
 Turing.setrdcache(true)
 
+##
+
+# Define sampling hyperparameters
+n_steps = 1000
+n_walkers = 4
 
 ##
 
 println("Loading data...\n")
 # Import data
-data = CSV.read("$(git_root())/test/data/data_example_02.csv", DF.DataFrame)
-# Add frequency column
-data[!, :freq] = data.count ./ data.count_sum
-
-##
-
-# Plot trajectories
-
-println("Plotting frequency trajectories...\n")
-# Initialize figure
-fig = Figure(resolution=(450, 350))
-
-# Add axis
-ax = Axis(
-    fig[1, 1],
-    xlabel="time point",
-    ylabel="barcode frequency",
-    yscale=log10,
-    title="frequency trajectories"
-)
-
-# Plot Mutant barcode trajectories
-BayesFitness.viz.bc_time_series!(
-    ax,
-    data[.!data.neutral, :];
-    quant_col=:freq,
-    zero_lim=1E-9,
-    zero_label="extinct",
-    alpha=0.25,
-    linewidth=2
-)
-
-# Plot Neutral barcode trajectories
-BayesFitness.viz.bc_time_series!(
-    ax,
-    data[data.neutral, :];
-    quant_col=:freq,
-    zero_lim=1E-9,
-    color=ColorSchemes.Blues_9[end],
-    alpha=0.9,
-    linewidth=2
-)
-
-fig
-
-##
-
-println("Plotting log-frequency ratio trajectories...\n")
-# Initialize figure
-fig = Figure(resolution=(450, 350))
-
-# Add axis
-ax = Axis(
-    fig[1, 1],
-    xlabel="time point",
-    ylabel="ln(fₜ₊₁/fₜ)",
-    title="log-frequency ratio"
-)
-
-# Plot log-frequency ratio of mutants
-BayesFitness.viz.logfreq_ratio_time_series!(
-    ax,
-    data[.!data.neutral, :];
-    freq_col=:freq,
-    alpha=0.25,
-    linewidth=2
-)
-
-# Plot log-frequency ratio of neutrals
-BayesFitness.viz.logfreq_ratio_time_series!(
-    ax,
-    data[data.neutral, :];
-    freq_col=:freq,
-    color=ColorSchemes.Blues_9[end],
-    alpha=1.0,
-    linewidth=2
-)
-
-fig
-
-##
+data = CSV.read("$(git_root())/test/data/data_example_01.csv", DF.DataFrame)
 
 # Define function parameters
 
 param = Dict(
     :data => data,
-    :n_walkers => 1,
-    :n_steps => 10,
-    :outputname => "./output/data_example_02_joint_fitness",
+    :n_walkers => n_walkers,
+    :n_steps => n_steps,
+    :outputname => "./output/data_example_01_$(n_steps)steps_$(lpad(n_walkers, 2, "0"))walkers.jld2",
     :model => BayesFitness.model.fitness_lognormal,
-    :multithread => false,
-    :sampler => Turing.HMC(0.05, 10)
+    :model_kwargs => Dict(
+        :λ_prior => [3.0, 3.0]
+    ),
+    :sampler => Turing.NUTS(0.65),
+    :ensemble => Turing.MCMCThreads(),
 )
 
 ##
